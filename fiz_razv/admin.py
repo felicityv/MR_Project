82d33f68c8c323e3da99e0dd0f31cf8c9e44c748
Irina Lastname <irina@example.com>
2025-11-27 21:02:30 +0300

    добавлены модели характеристика показателей, рост, вес, имт

--- fiz_razv/admin.py
+++ fiz_razv/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Patient
+from .models import Rost
+from .models import Ves
+from .models import Imt
+
 
 admin.site.register(Patient)
+admin.site.register(Rost)
+admin.site.register(Ves)
+admin.site.register(Imt)
--- fiz_razv/models.py
+++ fiz_razv/models.py
@@ -15,17 +15,37 @@ class Patient(models.Model):
     def __str__(self):
         return f'{self.surname} {self.name}'
 
+class Har_Pokaz(models.Model):
+    GENDER_CHOICES = {
+        "М": "Мужской",
+        "Ж": "Женский"
+    }
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES)
+    age = models.FloatField()
+    ME = models.FloatField()
+    SD = models.FloatField()
+    p3 = models.FloatField()
+    p10 = models.FloatField()
+    p25 = models.FloatField()
+    p50 = models.FloatField()
+    p75 = models.FloatField()
+    p90 = models.FloatField()
+    p97 = models.FloatField()
 
-# class Anthro_Pokaz(models.Model):
-#     pokazatel = models.CharField(max_length=10)
-#     izmerenie = models.CharField(max_length=10)
-
-#     def __str__(self):
-#         return f'{self.pokazatel}'
+    class Meta:
+        abstract=True  #позволяет не создавать таблицу в базе данных
     
-# class Percentile(models.Model):
+    def __str__(self):
+        return f'{self.gender} {self.age}'
+
+class Rost(Har_Pokaz):
+    pass
+
+class Ves(Har_Pokaz):
+    pass
+
+class Imt(Har_Pokaz):
+    pass
 
 
 
-#     def __str__(self):
-#         return f'{self.pokazatel}'
